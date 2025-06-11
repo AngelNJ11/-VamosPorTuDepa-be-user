0897194bddeaf6060bbe127d831e2b1d08c5f7fc
angelnj11 <angelnj733@gmail.com>
2025-06-10 23:44:28 -0500

    Configuración del proyecto usuario

---
+++ src/main/java/pe/vptd/VamosPorTuDepaBeUserApplication.java
@@ -0,0 +1,13 @@
+package pe.vptd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VamosPorTuDepaBeUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VamosPorTuDepaBeUserApplication.class, args);
+    }
+
+}
